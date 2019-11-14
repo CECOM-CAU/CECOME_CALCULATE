0adae3d4f354d460e8bb538c9b53cd586fb0bb92
unknown <LeeSangWon>
2019-11-14 20:46:50 +0900

    init

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+   public static void main(String[] args) {
+   
+      
+      Operand op1=new Operand("123456");
+      //op1.add("123456");
+      op1.subract("023356");
+      op1.print();
+   }
+
+}
